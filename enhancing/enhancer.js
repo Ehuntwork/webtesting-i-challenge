fc2d9c4b47d74ed413d8a12475d9f7e7358405d2
emilme <emilme@sir.mle>
2020-08-20 13:42:16 -0700

    mvp met

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,18 +5,53 @@ module.exports = {
   get,
 };
 
+
+
+//accepts an item and return enchantment success
+
+// The item's enhancement increases by 1.
+// If the item enhancement level is 20, the enhancement level is not changed.
+// The durability of the item is not changed.
+
 function success(item) {
-  return { ...item };
+  if(item.enchantment === 20){
+    return { ...item};
+  } else if(item.enchantment > 20){
+    return false
+  } else{
+    return { ...item, enchantment: item.enchantment+1}
+  }
 }
+
+
+//accepts an item and return enchantment failure
+
+// If the item's enhancement is less than 15, the durability of the item is decreased by 5.
+// If the item's enhancement is 15 or more, the durability of the item is decreased by 10.
+// If the item's enhancement level is greater than 16, the enhancement level decreases by 1 (17 goes down to 16, 18 goes down to 17).
 
 function fail(item) {
-  return { ...item };
+  if(item.enchantment < 15){
+      if(item.enchantment <= 5){
+        return { ...item, enchantment: 0}
+      } else{ return { ...item, enchantment: item.enchantment - 5};
+    }
+  } else if(item.enchantment > 16){
+    return { ...item, enchantment: item.enchantment - 1}
+  } else{
+    return { ...item}
+  }
 }
+
+
+//accepts item to return durability to 100
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100};
 }
 
+
+//stretch
 function get(item) {
   return { ...item };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,122 @@
 const enhancer = require('./enhancer.js');
 // test away!
+describe('enhnacer',()=>{
+    describe('repair()', ()=>{
+        it('should return durability to 100', ()=>{
+            // Items have name, durability and enhancement.
+            // The item's enhancement it's a number from 0 to 20.
+            // The item's durability it's a number from 0 to 100.
+
+            const item = {
+            name: '',
+            durability: 0,
+            enchantment: 0
+            }
+
+            const result = enhancer.repair(item)
+  
+            expect(result.durability).toEqual(100)
+        })
+    })
+
+    describe('successs()', ()=>{
+        it('should increase enchantment by one', ()=>{
+            // Items have name, durability and enhancement.
+            // The item's enhancement it's a number from 0 to 20.
+            // The item's durability it's a number from 0 to 100.
+
+            const item = {
+            name: '',
+            durability: 0,
+            enchantment: 3
+            }
+
+            const result = enhancer.success(item)
+  
+            expect(result.enchantment).toEqual(item.enchantment+1)
+        })
+
+        it('should not increase when 20', ()=>{
+            // Items have name, durability and enhancement.
+            // The item's enhancement it's a number from 0 to 20.
+            // The item's durability it's a number from 0 to 100.
+
+            const item = {
+            name: '',
+            durability: 0,
+            enchantment: 20
+            }
+
+            const result = enhancer.success(item)
+  
+            expect(result.enchantment).toEqual(item.enchantment)
+        })
+
+        it('should recieve error messege when over 20', ()=>{
+            // Items have name, durability and enhancement.
+            // The item's enhancement it's a number from 0 to 20.
+            // The item's durability it's a number from 0 to 100.
+
+            const item = {
+            name: '',
+            durability: 0,
+            enchantment: 25
+            }
+
+            const result = enhancer.success(item)
+  
+            expect(result).toEqual(false)
+        })
+    })
+
+    describe('fail()', ()=>{
+        it('should decrease by 5', ()=>{
+            // Items have name, durability and enhancement.
+            // The item's enhancement it's a number from 0 to 20.
+            // The item's durability it's a number from 0 to 100.
+
+            const item = {
+            name: '',
+            durability: 0,
+            enchantment: 8
+            }
+
+            const result = enhancer.fail(item)
+  
+            expect(result.enchantment).toEqual(item.enchantment - 5)
+        })
+
+        it('should not go bellow 0', ()=>{
+            // Items have name, durability and enhancement.
+            // The item's enhancement it's a number from 0 to 20.
+            // The item's durability it's a number from 0 to 100.
+
+            const item = {
+            name: '',
+            durability: 0,
+            enchantment: 5
+            }
+
+            const result = enhancer.fail(item)
+  
+            expect(result.enchantment).toEqual(0)
+        })
+
+        it('should decrease by 1', ()=>{
+            // Items have name, durability and enhancement.
+            // The item's enhancement it's a number from 0 to 20.
+            // The item's durability it's a number from 0 to 100.
+
+            const item = {
+            name: '',
+            durability: 0,
+            enchantment: 18
+            }
+
+            const result = enhancer.fail(item)
+  
+            expect(result.enchantment).toEqual(item.enchantment - 1)
+        })
+
+    })
+})
